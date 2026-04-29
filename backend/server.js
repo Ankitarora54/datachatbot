138dde4218d526f5b9147d6e31d238089ab2a9a2
ankitarora54 <ankitarora54@live.com>
2026-04-29 14:39:45 +0530

    frontend env setup

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,17 @@ const cors = require("cors");
 
 
 const app = express();
-app.use(cors({origin: "*",}));
+app.use(cors({origin: [
+    "http://localhost:5173",
+    "https://your-vercel-app.vercel.app"
+  ],}));
 // app.use(cors());
 app.use(express.json());
+
+/* HEALTH ROUTE */
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/query": "http://localhost:5000",
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/query": "http://localhost:5000",
+  //   },
+  // },
 });
